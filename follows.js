f95d3f707a9162a772fafa68dd65004220c923e1
Dimitris <dimitriskarageorgopoulos@gmail.com>
2015-03-01 23:28:43 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'ZKPRcyV1nMJUMlifIlWG9nWxcsO95Hy8hdCM96BSg92cmHGr', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2071969001], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Brite Solar', // Replaces Xively logo in the header
+		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
